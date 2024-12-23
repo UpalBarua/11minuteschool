923f577822403bd712a98162d8f18c4cbeb3d894
UpalBarua <upalbarua52@gmail.com>
2024-12-23 09:32:56 +0600

    add: initial project setup

---
+++ client/README.md
@@ -0,0 +1,1 @@
+# 11 Minutes School
---
+++ client/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function HomePage() {
+  return (
+    <div>
+      <h1 className="py-10 text-center text-3xl font-medium">
+        11 Minutes School
+      </h1>
+    </div>
+  );
+}
