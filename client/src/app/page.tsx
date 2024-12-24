f399f5335a81a2a47676228988c49c70916523cc
UpalBarua <upalbarua52@gmail.com>
2024-12-24 12:56:45 +0600

    add: offerings section

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Banner } from "@/components/banner";
+import { Offerings } from "@/components/offerings";
 
 export default function HomePage() {
   return (
     <div className="grid-container">
       <Banner />
+      <Offerings />
       <h1 className="py-10 text-center text-3xl font-medium">
         11 Minutes School
       </h1>
