1904fa4759f6ba0bf9ff7e55ef317d0bdf547751
UpalBarua <upalbarua52@gmail.com>
2024-12-25 08:26:36 +0600

    add: ssc and hsc section

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,11 +1,13 @@
 import { Banner } from "@/components/banner";
 import { Offerings } from "@/components/offerings";
+import { SscAndHsc } from "@/components/ssc-and-hsc";
 
 export default function HomePage() {
   return (
     <div className="grid-container">
       <Banner />
       <Offerings />
+      <SscAndHsc />
       <h1 className="py-10 text-center text-3xl font-medium">
         11 Minutes School
       </h1>
